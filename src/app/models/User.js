f6e0d59d828cbc63aaaf2492f98b360b310813ce
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-03-02 10:27:45 -0300

    Criptografia de senha

---
+++ src/app/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  this.password = await bcrypt.hash(this.password, 8)
+})
+
+module.exports = mongoose.model('User', UserSchema)
